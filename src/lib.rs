e67cba52ba94cf6f022124c434b6c89661dc296d
zTgx <747674262@qq.com>
2021-10-01 23:46:43 +0800

    add struct Staking

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,28 @@
+//! # Staking core data structure
+//!
+//! including Staking
+//!
+
+#[derive(Debug)]
+pub struct Staking {
+    /// enable staking or not
+    pub enable : bool,
+
+    /// current block height
+    pub block_height : u64,
+
+    /// validators at current block height
+    pub validators : Vec<String>, 
+
+    /// amount of Native is staking
+    pub weight : u64,
+
+    /// amount of redeem en the way
+    pub redeem : u64,
+
+    /// staking op transactions at current block height
+    pub txs : Vec<String>,
+}
 
 pub fn hello() {
     println!("Hello, staking!");
